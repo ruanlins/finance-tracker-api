c5a073b65c4424a4bc25c6de0426d205856f4007
Ruan Lins <ruanblins@gmail.com>
2025-03-31 19:09:10 +0000

    Created In Memory Transactions Repository

--- src/repositories/transactions-repository.ts
+++ src/repositories/transactions-repository.ts
@@ -4,6 +4,6 @@ export interface TransactionsRepository {
     findById(id: string): Promise<Transaction | null>
     findByUserId(id: string): Promise<Transaction[]>
     create(data: Prisma.TransactionUncheckedCreateInput): Promise<Transaction>
-    edit(data: Prisma.TransactionUpdateInput): Promise<Transaction>
+    edit(id: string,data: Prisma.TransactionUpdateInput): Promise<Transaction>
     delete(id: string): Promise<null>
 }

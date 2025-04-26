4d91cbb4ccae6aaed56298788f35e91253761796
Ruan Lins <ruanblins@gmail.com>
2025-04-26 03:22:43 +0000

    Created register route and e2e tests

---
+++ src/http/controllers/users/register.spec.ts
@@ -0,0 +1,43 @@
+import {describe,it,afterAll,beforeAll,expect} from 'vitest'
+import request from 'supertest'
+import { startTestDatabase, stopTestDatabase } from '@/utils/test/setupTestDatabase'
+
+let app: any
+
+describe('Register User e2e test', () => {
+
+  beforeAll(async () => {
+    await startTestDatabase()
+
+    const server = await import('@/app')
+    app = server.app
+  })
+
+  afterAll(async () => {
+    await stopTestDatabase()
+  })
+
+  it('should be able to register', async () => {
+    const res = await request(app).post('/users/register').send({
+      name: 'John Doe',
+      email: 'johndoe@test.com',
+      password: 'test@123',
+    })
+
+    expect(res.status).toBe(201)
+    expect(res.body.usuario.nome).toBe('John Doe')
+
+    console.log()
+  })
+
+  it('should not create another user with the same email', async () => {
+    const res = await request(app).post('/users/register').send({
+      name: 'John Doe',
+      email: 'johndoe@test.com',
+      password: 'test@123',
+    })
+
+    expect(res.status).toBe(409)
+  })
+
+})
--- src/http/controllers/users/register.ts
+++ src/http/controllers/users/register.ts
@@ -1,26 +1,62 @@
 import { z } from 'zod'
-import {Response,Request} from 'express'
+import { Request, Response, NextFunction } from 'express'
+import { RegisterUseCase } from '@/use-cases/users/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { PrismaWalletsRepository } from '@/repositories/prisma/prisma-wallets-repository'
+import jwt from 'jsonwebtoken'
+import { env } from '@/env'
+import { UserAlreadyExistsError } from '@/use-cases/erros/user-alreay-exists-error'
+import createHttpError from 'http-errors'
 
-export function userRegister(req:Request, res:Response) {
+export async function userRegister(req:Request, res:Response, next:NextFunction) {
     const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6)
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const parsedBody  = registerBodySchema.safeParse(req.body)
+  if (!parsedBody .success) {
+    res.status(400).json({
+      mensagem: 'Erro de validação',
+      erros: parsedBody.error.errors,
     })
+    return
+  }
 
-    const body = registerBodySchema.safeParse(req.body)
+    const { name, email, password } = parsedBody .data
 
-    if(!body.success) return res.status(400).json({mensagem: 'Erro de validação', erros: body.error.errors})
-
-    const {name,email,password} = body.data
-
+    
     try {
-        
-    } catch (error) {
-        
-    }
-
+        const usersRepository = new PrismaUsersRepository()
+        const walletsRepository = new PrismaWalletsRepository()
+        const registerUseCase = new RegisterUseCase(
+          usersRepository,
+          walletsRepository
+        )
     
-
+        const { user } = await registerUseCase.execute({ name, email, password })
     
-}
+        const token = jwt.sign({}, env.JWT_SECRET, { expiresIn: '1d' })
+    
+        res.cookie('token', token, {
+          httpOnly: env.NODE_DEV === 'production',
+          sameSite: 'strict',
+          maxAge: 1000 * 60 * 60 * 24,
+        })
+    
+        res.status(201).json({
+          mensagem: 'Usuário registrado com sucesso',
+          usuario: {
+            nome: user.name,
+            email: user.email,
+          },
+        })
+
+      } catch (err) {
+        if (err instanceof UserAlreadyExistsError) {
+          throw createHttpError(409, err.message)
+        }
+        next(err)
+      }
+  }
